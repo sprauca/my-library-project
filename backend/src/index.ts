23e81d3ee7f713ba65b4461a8d66319eb7e334ee
Darkskullz9 <praucasteven@gmail.com>
2025-07-18 16:16:44 +0200

    Ajout de la prise en charge de Keycloak pour l'authentification et mise à jour des dépendances express-session et keycloak-js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
+
 import userRoutes from './routes/user';
 import authRoutes from './routes/auth';
 import gameRoutes from './routes/game';
@@ -9,12 +12,34 @@ dotenv.config();
 
 const app = express();
 
+const memoryStore = new session.MemoryStore();
+
+app.use(session({
+    secret: 'secret sauce',
+    resave: false,
+    saveUninitialized: false,
+    store: memoryStore
+}));
+
+const keycloak = new Keycloak(
+    {store: memoryStore},
+    {
+        realm: process.env.KEYCLOAK_REALM || 'my-library-project',
+        'auth-server-url': process.env.KEYCLOAK_URL || 'http://localhost:8080/auth',
+        'ssl-required': 'external',
+        resource: process.env.KEYCLOAK_CLIENT_ID || 'backend',
+        'confidential-port': 0
+    }
+);
+
 app.use(cors());
 app.use(express.json());
+app.use(keycloak.middleware());
+
+app.use('/games', keycloak.protect(), gameRoutes);
 
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
-app.use('/games', gameRoutes);
 
 app.get('/', (_req, res) => {
     res.send('Welcome to the API backend!');
