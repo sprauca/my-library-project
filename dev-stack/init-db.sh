602c67cccfe2dbe6ebf5cd70b9b0fc748f051eec
Darkskullz9 <praucasteven@gmail.com>
2025-07-25 19:40:02 +0200

    refactor(security): remplacer les identifiants en dur par des variables d'environnement
    Cette modification remplace les identifiants codés en dur dans le fichier docker-compose.yml par des variables d'environnement pour améliorer la sécurité. Le script d'initialisation de la base de données a également été amélioré avec des messages plus clairs et une spécification explicite de la base de données.

--- dev-stack/init-db.sh
+++ dev-stack/init-db.sh
@@ -1,4 +1,7 @@
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+
+echo "Creating Keycloak DB..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
     CREATE DATABASE keycloakdb;
 EOSQL
